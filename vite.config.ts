c42d94ddedbbfc4caa5a89cb2f10ae4f122dd69b
Breno Luiz dos Santos <109548458+BrenoLSR@users.noreply.github.com>
2025-11-11 09:52:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Desafio-Frontend/', // <- caminho do seu repositório no GitHub
+  base: '/Desafio-Frontend/', // importante para GitHub Pages
 });
